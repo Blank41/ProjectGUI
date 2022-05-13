70c3e6e6a74f517f160f038e181a206c4c456f17
Blank <pluemnan@gmail.com>
2022-05-13 10:12:41 +0700

    Added Code Project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int income = int.Parse(txtIncome.Text);
+            int expenses = int.Parse(txtExpenses.Text);
+            int price = int.Parse(txtPrice.Text);
+            int day = price / (income - expenses);
+            txtDay.Text = day.ToString();
+
+        }
+    }
+}
